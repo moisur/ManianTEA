61c5b8ba4363dabfd8daac58cbe2b35abca28bd0
moisur <159698425+moisur@users.noreply.github.com>
2024-07-24 20:40:33 +0200

    Update index.js
    port 10000 comme indiqué sur RENDER

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb'); 
 
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT ||10000; 
 
 const uri = process.env.MONGODB_URI;
 
