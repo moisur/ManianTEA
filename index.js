6a4adbca30a0f3792c352a324e280b1ab94d86ba
moisur <159698425+moisur@users.noreply.github.com>
2024-08-01 17:08:12 +0200

    Update index.js
    test captcha

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const cors = require('cors');
 const express = require('express');
+const fetch = require('node-fetch');
 const { MongoClient, ServerApiVersion } = require('mongodb'); 
 
 const app = express();
-const port = process.env.PORT ||10000; 
+const port = process.env.PORT || 10000; 
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI; 
+const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY; 
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -14,19 +16,36 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-app.use(cors()); 
 
+app.use(cors()); 
 app.use(express.json()); 
 
 app.post('/api/subscribers', async (req, res) => {
     try {
-        await client.connect();
-        const db = client.db("Manian"); 
-        const subscribers = db.collection("Mails");
+        const { email, 'g-recaptcha-response': recaptchaToken } = req.body;
 
-        const { email } = req.body; 
-        const result = await subscribers.insertOne({ email });
-        res.status(201).json({ msg: 'Merci de votre inscription !' });
+        // 1. Validation de l'email
+        if (!validateEmail(email)) {
+            return res.status(400).json({ msg: 'Email invalide' }); 
+        }
+
+        // 2. Vérification du token reCAPTCHA 
+        const recaptchaResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptchaToken}`, { method: 'POST' });
+        const recaptchaData = await recaptchaResponse.json();
+
+        // 3. Vérification du score reCAPTCHA
+        if (recaptchaData.success && recaptchaData.score > 0.5) { 
+            await client.connect();
+            const db = client.db("Manian"); 
+            const subscribers = db.collection("Mails");
+
+            const result = await subscribers.insertOne({ email });
+            res.status(201).json({ msg: 'Merci de votre inscription !' });
+        } else {
+            // Score reCAPTCHA suspect
+            console.error("Requête suspecte bloquée (reCAPTCHA)");
+            res.status(400).json({ msg: 'Activité suspecte détectée' }); 
+        } 
     } catch (error) {
         console.error(error);
         res.status(500).json({ msg: 'Erreur serveur.' });
@@ -34,6 +53,11 @@ app.post('/api/subscribers', async (req, res) => {
         await client.close();
     }
 });
+
+function validateEmail(email) {
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+}
 
 app.listen(port, () => {
     console.log(`Serveur en écoute sur le port ${port}`);
