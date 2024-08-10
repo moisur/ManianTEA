c2ac21122634564f0fc61d766346954f520fa488
moisur <159698425+moisur@users.noreply.github.com>
2024-08-10 14:35:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const { RecaptchaEnterpriseServiceClient } = require('@google-cloud/recaptcha-enterprise');
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 10000; 
 
 const corsOptions = {
-  origin: 'https://moisur.github.io', 
+  origin: 'https://moisur.github.io', //  <-- Remplacez par votre domaine GitHub Pages
   optionsSuccessStatus: 200 
 };
+
 const uri = process.env.MONGODB_URI;
 const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY;
-const recaptchaSiteKey =  '6Ld37SIqAAAAALzGUoYS1444GFxYwR6OODSRm6V7'; // <-- Assurez-vous que c'est la bonne clé
+const recaptchaSiteKey =  '6Ld37SIqAAAAALzGUoYS1444GFxYwR6OODSRm6V7'; // <-- Vérifiez cette clé
 
 const recaptchaClient = new RecaptchaEnterpriseServiceClient();
 
-let db;
+let db; 
 
-// MongoDB Connection
+// MongoDB Connection 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -34,10 +35,13 @@ client.connect()
     })
     .catch((err) => {
         console.error("Erreur lors de la connexion à MongoDB", err);
+        // Gérer l'erreur de connexion à MongoDB, par exemple, arrêter l'application.
+        process.exit(1); 
     });
 
-app.use(cors());
-app.use(express.json());
+// Configuration du middleware CORS
+app.use(cors(corsOptions));
+app.use(express.json()); 
 
 app.post('/api/subscribers', async (req, res) => {
     try {
@@ -49,15 +53,15 @@ app.post('/api/subscribers', async (req, res) => {
             assessment: {
                 event: {
                     token: recaptchaToken,
-                    siteKey: recaptchaSiteKey, // <-- Utiliser la variable ici
-                    expectedAction: 'subscribe', // <-- Action correspondante
+                    siteKey: recaptchaSiteKey, 
+                    expectedAction: 'subscribe', 
                 },
             },
         });
 
         const recaptchaScore = recaptchaResponse[0].result.score;
 
-        if (recaptchaScore < 0.5) { 
+        if (recaptchaScore < 0.5) {
             return res.status(400).json({ msg: 'Validation reCAPTCHA échouée. Veuillez réessayer.' });
         }
 
